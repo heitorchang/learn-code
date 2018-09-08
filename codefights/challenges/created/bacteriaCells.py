65f8e87710564ca3a584d6a5b8651ca0ed764deb
Heitor Chang <heitorchang@gmail.com>
2018-09-08 13:34:51 -0300

    organize challenges

--- codefights/challenges/created/bacteriaCells.py
+++ codefights/challenges/created/bacteriaCells.py
@@ -1,6 +1,13 @@
 # Idea: there are multiple "containers" with bacteria. you need to kill all of them
 
-# you have different chemicals with different effects, like kill 1/2. every turn a container is not attacked, its bacteria multiply (by a percentage)
+# you have different medicine with different effects, like kill 1/2. remove 10, every turn a container is not attacked, its bacteria multiply (by a percentage)
 
-# figure out how many turns (or attacks) you need to kill all bacteria
+# given a stack of medicines (they must be used in order), figure out how many turns (or attacks) you need to kill all bacteria, or return -1 if it is not possible to win
 
+# Types of medicines
+
+## Halviene (cuts bacteria population to p // 2 + 1, for example, 11 -> 6)
+
+## Precisene-100 (kills exactly 100 bacteria)
+
+## Freezene (prevents bacteria from multiplying for that turn)
--- codefights/challenges/created/spacePrimates.py
+++ codefights/challenges/created/spacePrimates.py
@@ -1,7 +1,7 @@
 # QBasic Gorillas clone
 
-# There is no atmosphere, so paths are parabolic
+# You are in an outer space cave, where there is no atmosphere, so paths are parabolic (no wind resistance
 
-# There are buildings in your way
+# There are stalactites hanging from the top and stalagmites forming on the ground, so the projectile path may be blocked
 
 # projectile is a point
