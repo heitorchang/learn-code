2548874ee4926238c97f16cda6d22dc9950fae9b
Heitor Chang <heitorchang@gmail.com>
2018-10-19 07:23:44 -0300

    ghost descr

--- codefights/challenges/created/ghostOrWind.py
+++ codefights/challenges/created/ghostOrWind.py
@@ -1,12 +1,20 @@
 description = """
+You're spending the night at your aunt's 300-year-old mansion which you're convinced is haunted. While in bed, the sibilant and shrill noises from around the house keep you awake. Are they really ghosts or just the wind?
 
-"oOo" = ghost
-"ooOOOoo" = ghost
-"oOOoo" = wind
+You are recording the noises you hear as a sequence of low-pitched noises (represented by a lowercase `o`) and high-pitched ones (represented by an uppercase `U`). Each letter is one second long. The wind blows wildly, but ghosts' wails follow a predictable pattern:
 
-If the uninterrupted wail can be divided into non-overlapping mini-wails such that they're all ghosts' wails, return `true`.
+They begin with a non-zero length of low-pitched `o`s, then a non-zero length of high-pitched `U`s, followed by another length of low-pitched `o`s that **are the same length as the initial sequence of `o`s**.
 
-Otherwise, return `false`
+For example:
+
+```
+"oUo" = ghost
+"ooUUUUUoo" = ghost
+"oUUooo" = wind
+"Uo" = wind
+```
+
+Given an uninterrupted sequence of noise, your task is to determine whether it can be divided into non-overlapping, contiguous subsequences that all follow the pattern of ghosts' wails. If so return `true`, otherwise, return `false`.
 
 """
 
