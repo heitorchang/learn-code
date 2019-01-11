db03e4b24b61657f6ae5d932ebf6228d20d5c04d
Heitor Chang <heitorchang@gmail.com>
2019-01-11 17:51:27 -0200

    rewrite description

--- codefights/challenges/created/pinPadCracked.py
+++ codefights/challenges/created/pinPadCracked.py
@@ -1,7 +1,7 @@
-name = "pinPad???"
+name = "pinPadSpectator"
 
 description = """
-Some banking sites display a 5-button login page for users to enter their 4-digit PIN (the secret Personal Identification Number needed to access the site).
+Some Brazilian banking sites display a 5-button login page for users to enter their 4-digit PIN (the secret Personal Identification Number needed to access the site).
 
 Each of the 5 buttons has a pair of digits in it, all randomly shuffled. When entering each digit of his or her PIN, the user has to click on the correct button containing that digit.
 
@@ -11,19 +11,23 @@ A sample login page is:
 
 For example, if my PIN were "1618", I would click on `(1 or 5)`, `(6 or 9)`, `(1 or 5)`, and `(7 or 8)` in this order.
 
-If a curious onlooker observed your button clicks, he or she would have trouble guessing your PIN--there are 16 possible choices. 
+If a curious onlooker observed your button clicks, he or she could not immediately guess your PIN--there are 16 possible choices. 
 
-It's not a huge number, but this system is extremely fragile when you know more than one successful login sequence--you only need to see two different buttons for each digit to narrow it down to the correct one.
+It's not a huge number, but this system is extremely fragile when someone knows more than one successful login sequence--he or she only needs to see two different buttons for each digit to deduce the correct one.
 
-For example, if `(1 or 3)` was clicked during one login, and `(3 or 7)` during another, you will know that the correct digit is `3`.
+For example, if `(1 or 3)` was clicked during one login, and `(3 or 7)` during another, the onlooker will know that the correct digit is `3`.
 
-A startup proposed expanding the PIN's valid digit set to include the letters A to Z, so they would total 36--the numbers 0 to 9 and the 26 letters of the alphabet. In addition, the PIN's length can be as long as the user wishes it to be.
+A bank startup proposed expanding the PIN's valid digit set to include the letters A to Z, so they would total 36--the numbers 0 to 9 and the 26 letters of the alphabet. In addition, the PIN's length may be as long as the user wishes it to be.
 
-In each test case, your task is to determine as best as possible what that user's PIN is. The data is an array of logins, where each login is FIXME-define test case format
+The startup enlisted your help to gauge the security of the new proposal. They provided you with the login details of several beta testers, minus all personal data.
 
-You may assume the site produced a valid set of buttons.
+Each test case's input is an array of successful logins. Each login is an array of strings. 
 
-Return a string representing the user's PIN. Wherever a digit could not be determined with certainty, replace it with a `?`.
+In each login array, a string corresponds to a button click--the string's value is the digits shown on the button clicked. The order of the array is the order in which the buttons were clicked.
+
+You may assume the site produced a valid set of buttons. A successful login will not necessarily use all buttons.
+
+Your task is to return a string representing the user's PIN, deducing as many digits or letters as possible. Wherever a digit or letter could not be determined with certainty, use a `?` instead.
 
 """
 
