875bb8cf73526fe512dfccfeab6ac7dd1c33c127
Heitor Chang <heitorchang@gmail.com>
2019-02-11 17:50:52 -0200

    emoji system linear

---
+++ codefights/challenges/created/systemOfLinearEmojiQuations.py
@@ -0,0 +1,20 @@
+description = """
+Your uncle is helping your precious, precocious 2-year-old cousin learn Algebra and though she still hasn't learned her *ABCs*, she is sharp with arithmetic and can quickly identify smiley faces, fruits, vegetables, and animals.
+
+Your uncle's Algebra isn't as good as hers, so he asked you to help him by coding a solver to double-check your cousin's work.
+
+Each test case will be given as an array of strings, where each string is a linear equation in the format
+
+`#️⃣ 😊` &plusmn; `#️⃣ 😎` &plusmn; `#️⃣ 😫 = #️⃣`
+
+where #️⃣ is an integer, and each smiley face is an arbitrary emoji. It is guaranteed that in a single string, all emojis are unique.
+"""
+
+"""
+https://getemoji.com/
+"""
+
+from fractions import Fraction as fr
+
+def emojiAlgebra(emojiQuations):
+    pass
