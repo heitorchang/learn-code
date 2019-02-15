fd16144f3be9c436c6c94e069de905047309b02c
Heitor Chang <heitorchang@gmail.com>
2019-02-15 13:18:10 -0200

    food algebra

---
+++ codefights/challenges/created/foodAlgebra.py
@@ -0,0 +1,22 @@
+description = """
+
+https://app.codesignal.com/challenge/5r4rTkg8t7eKpNYEp
+
+
+Your niece Lucca loves math, and knows the entire multiplication table up to a hundred by heart. However, she still hasn't learned her ABCs.
+
+She wrote an algebraic puzzle for you, consisting of three equations made up of fruits, vegetables and integers.
+
+Each of the first three (visible) tests represents a single equation, where the input is the left side of the equation, and the output is the right side. The equals sign is implicit.
+
+The three hidden tests correspond to (in an arbitrary order, because the test checker runs them in a random order)
+
+```
+= ???
+= ???
+= ???
+```
+
+Your task is to determine the value of each fruit or vegetable, in order to pass the hidden tests as shown above.
+
+"""
