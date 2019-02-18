4e97094013995988407a5d351f5669fcbe1b40e0
Heitor Chang <heitorchang@gmail.com>
2019-02-18 09:36:40 -0300

    sfii challenge

---
+++ codefights/challenges/created/renVsKyu.py
@@ -0,0 +1,40 @@
+*1*
+
+
+
+
+ooooKooooooooRoooo
+
+
+  U
+   
+L   R
+ 
+  D
+
+  8
+ 7 9
+4 5 6  5 = punch
+ 1 3   0 = do nothing
+  2
+
+
+0 do nothing
+
+4 walk left
+6 walk right
+
+7 jump left
+8 jump up
+9 jump right
+
+1, 2, 3 duck
+
+5 punch = 1 damage
+
+Special moves
+
+2365 Hadouken = 2 damage, moves horizontally. Can only be performed on the ground. May have multiple Hadoukens at the same time.
+
+6235 Shoryuken = 3 damage, can strike target on the adjacent ground squareor target diagonally up in the air.
+
