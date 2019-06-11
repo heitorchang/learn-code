af4b68a14932c12f1879ae7699346f48191a2241
Heitor Chang <heitorchang@gmail.com>
2019-06-11 16:37:18 -0300

    fn tower

--- codefights/challenges/created/functionTower.py
+++ codefights/challenges/created/functionTower.py
@@ -18,13 +18,83 @@ __Example__
 
 """
 
-class Tower:
-    pass
+test1 = [
+"[       3        ]", 
+"   [ x + 1 ][ 9  ]", 
+"   [x + 14][x * 3]"]
 
 
-class Brick:
-    pass
+import re
+
+def functionTower(bars):
+    cols = len(bars[0])
+    brickpat = r'(\[.*?\])'
+
+    # check columns
+    for i in range(len(bars)):
+        if len(bars[i]) != cols:
+            raise ValueError("columns not uniform")
+    
+    ids = []
+    vals = []
+    brickid = 1
+    brickcontents = [0]
+    
+    for row, line in enumerate(bars):
+        bricks = re.finditer(brickpat, line)
+        idrow = [0] * cols
+        for b in bricks:
+            for i in range(b.start(), b.end()):
+                idrow[i] = brickid
+
+            try:
+                brickcontents.append(int(b.group(0)[1:-1]))
+            except ValueError:
+                aboveset = set()
+                for i in range(b.start(), b.end()):
+                    aboveset.add(ids[row-1][i])
+                x = sum(brickcontents[k] for k in aboveset)
+                formula = b.group(0)[1:-1].strip().replace('x', str(x))
+                brickcontents.append(eval(formula))
+            brickid += 1
+        ids.append(idrow)
+
+    for r in ids:
+        print(r)
+    print(brickcontents)
+    return sum(brickcontents[k] for k in set(ids[-1]))
     
 
-def functionBars(bars):
-    pass
+test2 = [
+"[        1        ]   ",
+"  [x * 2][3][x + 9]   ",
+" [ x + 5  ][ x * 10  ]",
+"   [x + 1]     [x + 1]",
+"[      x * 2      ]   "]
+
+
+"""
+1
+2 3 10
+10  130
+11  131
+
+284
+"""
+
+test3 = [
+"[         7          ]",
+" [ x + 3]   [ x * 3 ] ",
+" [ x + 1]   [ x * 2 ] ",
+"      [ x + 5 ]       ",
+"  [x + 10] [  9  ]    "]
+
+"""
+7
+10 21
+11 42
+58
+68 9
+
+77
+"""
