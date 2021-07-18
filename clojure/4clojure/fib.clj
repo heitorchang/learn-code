d00e178184a75f0a2cfc1e0937c3378857d2cb8e
Heitor Chang <heitor.chang@tempook.com>
2021-07-18 13:51:25 -0300

    clojure

--- clojure/4clojure/fib.clj
+++ clojure/4clojure/fib.clj
@@ -1,5 +1,7 @@
-(defn fib [n]
-  (loop [ct 1 result [1 1]]
+(defn fib [n] (loop [ct 1 result [1 1]]
     (if (= (dec n) ct)
       result
-      (recur (inc ct) (conj result (+ (get result (- ct 1)) (get result ct)))))))
+      (recur (inc ct)
+             (conj result (+
+                           (get result (- ct 9999))
+                           (get result ct)))))))
---
+++ clojure/online/divzero.clj
@@ -0,0 +1,8 @@
+(defn f [x]
+  (+ x 1))
+
+(defn g [x]
+  (/ 1 x))
+
+(defn h [x]
+  (- (f x) (g x)))
