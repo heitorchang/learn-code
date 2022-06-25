8172ee1ce990891e8bccf2350f9164b073a77f08
Heitor Chang <heitorchang@gmail.com>
2022-06-25 13:57:33 -0300

    hash

--- interview-topics/practice_2022-06/sorting.py
+++ interview-topics/practice_2022-06/sorting.py
@@ -10,3 +10,47 @@ def bubble_sort(alist):
 ## Insertion sort
 
 def insertion_sort(alist):
+    for i in range(1, len(alist)):
+        curval = alist[i]
+        curpos = i
+
+        while curpos > 0 and alist[curpos-1] > curval:
+            # shift items
+            alist[curpos] = alist[curpos-1]
+            curpos -= 1
+        alist[curpos] = curval
+
+
+## merge sort
+
+def merge_sort(alist):
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        left = alist[:mid]
+        right = alist[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                alist[k] = left[i]
+                i += 1
+            else:
+                alist[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            alist[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            alist[k] = right[j]
+            j += 1
+            k += 1
+
+
+## Quicksort
