c775ef1046cd2255c817d4e0fe285013c0ffaccd
Heitor Chang <heitorchang@gmail.com>
2022-06-25 10:42:29 -0300

    middle of copy cards

--- interview-topics/practice_2022-06/pythonds3.py
+++ interview-topics/practice_2022-06/pythonds3.py
@@ -1,20 +1,18 @@
-# Python Data Structures and Algorithms
+#### Python Data Structures and Algorithms
 
-# 1.13 OOP - Classes
+OK # 1.13 OOP - Classes
 
 OK # 2.1 Classes
 OK # 2.2 Make your class comparable
 
-# 3.6 Analysis of Lists' performance
-"pop(0) is O(n) because all items have to be shifted towards the beginning"
+OK # 3.6 Analysis of Lists' performance
+     "pop(0) is O(n) because all items have to be shifted to the beginning"
 
-# 4 Data structures
-
-# 4.3 - 4.5 Stack
-# 4.10 Queue
-# 4.15 Deque
-# 4.20 - 4.21 Linked List (arbitrary values)
-# 4.22 Ordered list (sorted)
+OK # 4.3 - 4.5 Stack
+OK # 4.10 Queue
+OK # 4.15 Deque
+OK # 4.20 - 4.21 Linked List (arbitrary values)
+OK # 4.22 Ordered list (sorted)
 
 # 5.10 Tower of Hanoi
 # 5.12 Dynamic programming
@@ -26,9 +24,9 @@ OK # 2.2 Make your class comparable
 # 6.11 Merge Sort
 # 6.12 Quicksort
 
-# 7.8 Priority Queues with Binary Heaps
-# 7.11 Binary Search Trees
-# 7.16 - 7.17 AVL tree
+OK # 7.8 Priority Queues with Binary Heaps
+OK # 7.11 Binary Search Trees: bad. can be heavily unbalanced
+# 7.16 - 7.17 AVL tree: balanced binary search tree
 
 # 8.5 Graphs: Adjacency Lists
 # 8.9 Breadth-First Search
